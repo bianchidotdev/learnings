a077f233bb69f386e57b9a65cfeab8899cb7fa82
Michael Bianchi <michael@bianchi.dev>
2022-04-18 21:58:51 -0500

    add exdoc to test deps

--- elixir_book/mix.exs
+++ elixir_book/mix.exs
@@ -36,7 +36,7 @@ defmodule ElixirBook.MixProject do
       {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.0", only: [:dev, :test], runtime: false},
       {:excoveralls, "~> 0.10", only: :test},
-      {:ex_doc, "~> 0.27", only: :dev, runtime: false},
+      {:ex_doc, "~> 0.27", only: [:dev, :test], runtime: false},
       {:stream_data, "~> 0.5", only: [:dev, :test]}
     ]
   end
