2170041d30cf8cc9f74b0249db42fa6b70cd4432
Bianchi <michael@bianchi.dev>
2023-12-02 00:06:14 -0600

    format

--- elixir_book/test/fizz_buzz_test.exs
+++ elixir_book/test/fizz_buzz_test.exs
@@ -4,6 +4,22 @@ defmodule FizzBuzzTest do
 
   # ch12 ControlFlow-1
   test "returns fizzbuzz results up 15" do
-    assert FizzBuzz.upto(15) == [1, 2, "fizz", 4, "buzz", "fizz", 7, 8, "fizz", "buzz", 11, "fizz", 13, 14, "fizzbuzz"]
+    assert FizzBuzz.upto(15) == [
+             1,
+             2,
+             "fizz",
+             4,
+             "buzz",
+             "fizz",
+             7,
+             8,
+             "fizz",
+             "buzz",
+             11,
+             "fizz",
+             13,
+             14,
+             "fizzbuzz"
+           ]
   end
 end
