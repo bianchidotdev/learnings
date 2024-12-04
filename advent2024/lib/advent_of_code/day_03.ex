0053fbf0b03697affdc423739d3713e3df318e98
Bianchi <m@bianchi.dev>
2024-12-03 20:02:54 -0600

    Day 3 solution

--- advent2024/lib/advent_of_code/day_03.ex
+++ advent2024/lib/advent_of_code/day_03.ex
@@ -1,7 +1,34 @@
 defmodule AdventOfCode.Day03 do
-  def part1(_args) do
+  def part1(input) do
+    parsed = parse(input)
+
+    Regex.scan(~r/mul\(\d{1,3},\d{1,3}\)/, parsed)
+    |> Enum.map(fn [match] -> calc_mul(match) end)
+    |> Enum.sum()
   end
 
-  def part2(_args) do
+  def part2(input) do
+    parsed = parse(input)
+
+    [first | dont_parts] = String.split(parsed, "don't()")
+    do_parts = dont_parts
+    |> Enum.map(fn part -> String.split(part, "do()", parts: 2) end)
+    |> Enum.map(&Enum.at(&1, 1))
+    |> Enum.filter(&(&1))
+
+    [first | do_parts]
+    |> Enum.flat_map(fn part -> Regex.scan(~r/mul\(\d{1,3},\d{1,3}\)/, part) end)
+    |> Enum.map(fn [match] -> calc_mul(match) end)
+    |> Enum.sum()
+  end
+
+  defp parse(input) do
+    input
+    |> String.trim()
+  end
+
+  defp calc_mul(match) do
+    captures = Regex.named_captures(~r/mul\((?<a>\d{1,3}),(?<b>\d{1,3})\)/, match)
+    String.to_integer(captures["a"]) * String.to_integer(captures["b"])
   end
 end
