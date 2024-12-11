950128f0f8bd1a4e62f67d311a1729d8fb77ea57
Bianchi <m@bianchi.dev>
2024-12-11 17:31:55 -0600

    Solve day 11 part 2

--- advent2024/lib/advent_of_code/day_11.ex
+++ advent2024/lib/advent_of_code/day_11.ex
@@ -1,10 +1,13 @@
 defmodule AdventOfCode.Day11 do
   def part1(input, blinks \\ 25) do
+    # {:ok, cache} = GenServer.start_link(AdventOfCode.Day11.Cache, [])
     parsed = parse_input(input)
 
     parsed
+    |> Enum.reduce(%{}, fn stone, acc -> Map.update(acc, stone, 1, &(&1 + 1)) end)
     |> blink(blinks)
-    |> Enum.count()
+    |> Enum.map(fn {_k, v} -> v end)
+    |> Enum.sum()
   end
 
   def part2(input) do
@@ -14,23 +17,36 @@ defmodule AdventOfCode.Day11 do
   defp blink(stones, 0), do: stones
 
   defp blink(stones, blinks) do
-    stones
-    |> Enum.reduce([], fn stone, acc ->
-      stone_string = Integer.to_string(stone)
+    new_stones =
+      stones
+      |> Enum.reduce(%{}, fn {stone, count}, acc ->
+        case calc_new_stones(stone) do
+          {a, b} ->
+            acc = Map.update(acc, a, count, &(&1 + count))
+            Map.update(acc, b, count, &(&1 + count))
 
-      cond do
-        stone == 0 ->
-          [1 | acc]
+          new_stone ->
+            Map.update(acc, new_stone, count, &(&1 + count))
+        end
+      end)
 
-        Integer.mod(String.length(stone_string), 2) == 0 ->
-          {a, b} = String.split_at(stone_string, String.length(stone_string) |> div(2))
-          [String.to_integer(a), String.to_integer(b) | acc]
+    blink(new_stones, blinks - 1)
+  end
 
-        true ->
-          [stone * 2024 | acc]
-      end
-    end)
-    |> blink(blinks - 1)
+  defp calc_new_stones(stone) do
+    stone_string = Integer.to_string(stone)
+
+    cond do
+      stone == 0 ->
+        1
+
+      Integer.mod(String.length(stone_string), 2) == 0 ->
+        {a, b} = String.split_at(stone_string, String.length(stone_string) |> div(2))
+        {String.to_integer(a), String.to_integer(b)}
+
+      true ->
+        stone * 2024
+    end
   end
 
   defp parse_input(input) do
--- advent2024/test/advent_of_code/day_11_test.exs
+++ advent2024/test/advent_of_code/day_11_test.exs
@@ -5,13 +5,17 @@ defmodule AdventOfCode.Day11Test do
 
   @input "125 17"
 
+  test "part1_sample" do
+    result = part1("125 17", 6)
+    assert result == 22
+  end
+
   test "part1" do
     result = part1(@input)
 
     assert result == 55312
   end
 
-  @tag :skip
   test "part2" do
     result = part2(@input)
 
